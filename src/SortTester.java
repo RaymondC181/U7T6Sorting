68b3f138c429a377d9b26f6e65874715ed0e89e2
BT_1E10_03 <sample@bths.edu>
2022-02-18 10:53:52 -0500

    finished ec

--- src/SortTester.java
+++ src/SortTester.java
@@ -14,6 +14,13 @@ public class SortTester
 //    Sort.insertionSort(arr2);
 //    System.out.println(Arrays.toString(arr2));
 
+      int[] arr2 = {12, 6, 79, 34, 7, 2};
+      Sort.selectionSortDescending(arr2);
+      System.out.println(Arrays.toString(arr2));
+
+      int[] arr3 = {21, 45, 31, -56, 24, 90};
+      Sort.insertionSortDescending(arr3);
+      System.out.println(Arrays.toString(arr3));
 
   }
 }
